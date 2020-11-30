d50492e2a26fdf261b7f07536b3176ed10712157
Kamran <vkamran7@gmail.com>
2020-11-30 18:39:44 +0400

    first commit

---
+++ src/main/java/az/brandbook/BrandbookApplication.java
@@ -0,0 +1,13 @@
+package az.brandbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/brandbook/BrandbookApplicationTests.java
@@ -0,0 +1,13 @@
+package az.brandbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
